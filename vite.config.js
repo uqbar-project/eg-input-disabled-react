1c0148093b3869cb2c4030f89f50e28e3ca473b9
Fernando Dodino <fernando.dodino@gmail.com>
2023-11-14 21:07:46 -0300

    Fix definition for  vite and jsconfig

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",      
+    },
+  },
   test: {
     globals: true,
     setupFiles: ["./setupTests.js"],
     environment: 'jsdom',
     coverage: {
-      provider: 'v8',
       reporter: ['text', 'json', 'html', 'json-summary'],
     },
   }
